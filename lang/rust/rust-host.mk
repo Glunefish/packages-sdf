fcc1075a298b32c6bd52d15550b16ce78d24d9ae
GruntFish <fbyu7@qq.com>
2024-02-29 14:00:44 +0800

    Update rust-host.mk

--- lang/rust/rust-host.mk
+++ lang/rust/rust-host.mk
@@ -2,57 +2,50 @@
 #
 # Copyright (C) 2023 Luca Barbato and Donald Hoskins
 
-# Rust Environmental Vars
-CONFIG_HOST_SUFFIX:=$(word 4, $(subst -, ,$(GNU_HOST_NAME)))
-RUSTC_HOST_ARCH:=$(HOST_ARCH)-unknown-linux-$(CONFIG_HOST_SUFFIX)
-CARGO_HOME:=$(STAGING_DIR_HOST)/cargo
+# Variables (all optional) to be set in package Makefiles:
+#
+# RUST_HOST_FEATURES - list of options, default empty
+#
+#   Space or comma separated list of features to activate
+#
+#   e.g. RUST_HOST_FEATURES:=enable-foo,with-bar
 
-# Support only a subset for now.
-RUST_ARCH_DEPENDS:=@(aarch64||arm||i386||i686||mips||mipsel||mips64||mips64el||mipsel||powerpc64||x86_64)
-
-# Common Build Flags
-RUST_BUILD_FLAGS = \
-  CARGO_HOME="$(CARGO_HOME)"
-
-# This adds the rust environmental variables to Make calls
-MAKE_FLAGS += $(RUST_BUILD_FLAGS)
-
-# Force linking of the SSP library
-ifdef CONFIG_PKG_CC_STACKPROTECTOR_REGULAR
-  ifeq ($(strip $(PKG_SSP)),1)
-    RUSTC_LDFLAGS += -lssp_nonshared
-  endif
+ifeq ($(origin RUST_INCLUDE_DIR),undefined)
+  RUST_INCLUDE_DIR:=$(dir $(lastword $(MAKEFILE_LIST)))
 endif
-ifdef CONFIG_PKG_CC_STACKPROTECTOR_STRONG
-  ifeq ($(strip $(PKG_SSP)),1)
-    TARGET_CFLAGS += -lssp_nonshared
-  endif
-endif
+include $(RUST_INCLUDE_DIR)/rust-values.mk
 
-# mips64 openwrt has a specific targed in rustc
-ifeq ($(ARCH),mips64)
-  RUSTC_TARGET_ARCH:=$(REAL_GNU_TARGET_NAME)
-else
-  RUSTC_TARGET_ARCH:=$(subst openwrt,unknown,$(REAL_GNU_TARGET_NAME))
-endif
+CARGO_HOST_VARS= \
+	$(CARGO_HOST_CONFIG_VARS) \
+	CC=$(HOSTCC_NOCACHE) \
+	MAKEFLAGS="$(HOST_JOBS)"
 
-RUSTC_TARGET_ARCH:=$(subst muslgnueabi,musleabi,$(RUSTC_TARGET_ARCH))
+# $(1) path to the package (optional)
+# $(2) additional arguments to cargo (optional)
+define Host/Compile/Cargo
+	+$(CARGO_HOST_VARS) \
+	cargo install -v \
+		--profile $(CARGO_HOST_PROFILE) \
+		$(if $(RUST_HOST_FEATURES),--features "$(RUST_HOST_FEATURES)") \
+		--root $(HOST_INSTALL_DIR) \
+		--path "$(HOST_BUILD_DIR)/$(if $(strip $(1)),$(strip $(1)))" \
+		$(if $(filter --jobserver%,$(HOST_JOBS)),,-j1) \
+		$(2)
+endef
 
-ifeq ($(ARCH),i386)
-  RUSTC_TARGET_ARCH:=$(subst i486,i586,$(RUSTC_TARGET_ARCH))
-endif
+define Host/Uninstall/Cargo
+	+$(CARGO_HOST_VARS) \
+	cargo uninstall -v \
+		--root $(HOST_INSTALL_DIR) \
+		|| true
+endef
 
-# ARM Logic
-ifeq ($(ARCH),arm)
-  ifeq ($(CONFIG_arm_v7),y)
-    RUSTC_TARGET_ARCH:=$(subst arm,armv7,$(RUSTC_TARGET_ARCH))
-  endif
+define RustBinHostBuild
+  define Host/Install
+	$(INSTALL_DIR) $(STAGING_DIR_HOSTPKG)/bin
+	$(INSTALL_BIN) $(HOST_INSTALL_DIR)/bin/* $(STAGING_DIR_HOSTPKG)/bin/
+  endef
+endef
 
-  ifeq ($(CONFIG_HAS_FPU),y)
-    RUSTC_TARGET_ARCH:=$(subst musleabi,musleabihf,$(RUSTC_TARGET_ARCH))
-  endif
-endif
-
-ifeq ($(ARCH),aarch64)
-    RUST_CFLAGS:=-mno-outline-atomics
-endif
+Host/Compile=$(call Host/Compile/Cargo)
+Host/Uninstall=$(call Host/Uninstall/Cargo)
